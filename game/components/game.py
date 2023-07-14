59ccd4f30bb9fba261a4f2734322e66658a048e4
Miguel Ramírez <100735523+Migue04fuentes@users.noreply.github.com>
2023-07-14 01:18:03 -0500

    Class1: background,spaceship,motion

--- game/components/game.py
+++ game/components/game.py
@@ -1,9 +1,9 @@
 import pygame
 
 from game.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, DEFAULT_TYPE
+from game.components.spaceship import Spaceship
 
-
-class Game:
+class Game():
     def __init__(self):
         pygame.init()
         pygame.display.set_caption(TITLE)
@@ -14,6 +14,7 @@ class Game:
         self.game_speed = 10
         self.x_pos_bg = 0
         self.y_pos_bg = 0
+        self.player = Spaceship()
 
     def run(self):
         # Game loop: events - update - draw
@@ -31,13 +32,14 @@ class Game:
                 self.playing = False
 
     def update(self):
-        pass
+        user_input = pygame.key.get_pressed()
+        self.player.update(user_input)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
-        pygame.display.update()
+        self.player.draw(self.screen)
         pygame.display.flip()
 
     def draw_background(self):
